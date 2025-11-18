35b70afeee0fb105c831fb140b63b72affe392ec
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2025-11-17 21:10:57 -0300

    fiat: configuracao knex banco de dados

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,15 @@
-import pkg from "pg";
-const { Pool } = pkg;
+import knex from "knex";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = new Pool({
-	user: process.env.PG_USER,
-	password: process.env.PG_PASSWORD,
-	host: process.env.PG_HOST,
-	port: process.env.PG_PORT,
-	database: process.env.PG_DATABASE,
+export const db = knex({
+	client: "pg",
+	connection: {
+		host: process.env.PG_HOST,
+		user: process.env.PG_USER,
+		password: process.env.PG_PASSWORD,
+		database: process.env.PG_DATABASE,
+		port: process.env.PG_PORT,
+	},
 });
